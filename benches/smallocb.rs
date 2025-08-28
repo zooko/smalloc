a96c7237929a4f598b51dee947cc5f951e513c4b
Zsomething <nothanks@ontheemail.com>
2025-08-27 21:27:32 -0700

    add comments explaining how to use tango to bench this

--- benches/smallocb.rs
+++ benches/smallocb.rs
@@ -22,8 +22,8 @@ use rand::Rng;
 use tango_bench::Bencher;
 
 fn smallocb_benchmarks() -> impl IntoBenchmarks {
-    //let mut s = GlobalAllocWrap;
-    let mut s = Smalloc::new();
+    let mut s = GlobalAllocWrap; // comment-in for tango baseline
+    //let mut s = Smalloc::new(); // comment-in to compare to smalloc
 
     let l = Layout::from_size_align(35, 32).unwrap();
 
