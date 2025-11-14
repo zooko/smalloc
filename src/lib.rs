908eb2fa2fe6cca97088944ae065caa32568e48c
Zsomething <nothanks@ontheemail.com>
2025-11-14 00:08:36 -0700

    tweak

--- src/lib.rs
+++ src/lib.rs
@@ -786,7 +786,7 @@ pub mod benchmarks {
         assert!(end > start);
         let elap_ns = (end - start).as_nanos() as u64;
         let ns_per_iter = elap_ns / iters;
-        println!("name: {name:>12}, iters: {:>11}, ns: {:>12}, ns/i: {:>11}", iters.separate_with_commas(), elap_ns.separate_with_commas(), ns_per_iter.separate_with_commas());
+        println!("name: {name:>12}, iters: {:>11}, ns: {:>13}, ns/i: {:>11}", iters.separate_with_commas(), elap_ns.separate_with_commas(), ns_per_iter.separate_with_commas());
 
         elap_ns
     }
