ed1946b8f90e4aea20aedf44ed49d45325c5e0d3
Zsomething <nothanks@ontheemail.com>
2025-12-23 10:19:55 -0600

    tests: remove some ancient and unnecessary tests

--- smalloc/tests/integration.rs
+++ smalloc/tests/integration.rs
@@ -7,67 +7,6 @@ use devutils::nextest_integration_tests;
 use devutils::get_devsmalloc;
 
 nextest_integration_tests! {
-    fn one_alloc_and_dealloc_medium() {
-        let l = Layout::from_size_align(120, 4).unwrap();
-        let p = unsafe { get_devsmalloc!().alloc(l) };
-        assert!(!p.is_null());
-        unsafe { get_devsmalloc!().dealloc(p, l) };
-    }
-
-    fn one_realloc_to_tiny() {
-        let l = Layout::from_size_align(1, 1).unwrap();
-        let p = unsafe { get_devsmalloc!().alloc(l) };
-        let p2 = unsafe { get_devsmalloc!().realloc(p, l, 3) };
-        debug_assert_eq!(p, p2);
-    }
-
-    fn one_alloc_and_dealloc_small() {
-        let l = Layout::from_size_align(6, 1).unwrap();
-        let p = unsafe { get_devsmalloc!().alloc(l) };
-        unsafe { get_devsmalloc!().dealloc(p, l) };
-    }
-
-    fn one_alloc_and_dealloc_large() {
-        let l = Layout::from_size_align(1_000_000, 8).unwrap();
-        let p = unsafe { get_devsmalloc!().alloc(l) };
-        unsafe { get_devsmalloc!().dealloc(p, l) };
-    }
-
-    fn one_large_alloc_and_realloc_to_large() {
-        let l1 = Layout::from_size_align(1_000_000, 8).unwrap();
-        let p1 = unsafe { get_devsmalloc!().alloc(l1) };
-        assert!(!p1.is_null());
-
-        let reqsize: usize = 2_000_000;
-        let p2 = unsafe { get_devsmalloc!().realloc(p1, l1, reqsize) };
-        assert!(!p2.is_null());
-    }
-
-    fn one_medium_alloc_and_realloc_to_large() {
-        let l1 = Layout::from_size_align(300, 8).unwrap();
-        let p1 = unsafe { get_devsmalloc!().alloc(l1) };
-        assert!(!p1.is_null());
-
-        let reqsize: usize = 2_000_000;
-        let p2 = unsafe { get_devsmalloc!().realloc(p1, l1, reqsize) };
-        assert!(!p2.is_null());
-    }
-
-    fn one_large_alloc_and_realloc_to_oversize() {
-        let l1 = Layout::from_size_align(1_000_000, 8).unwrap();
-        let p1 = unsafe { get_devsmalloc!().alloc(l1) };
-        assert!(!p1.is_null());
-
-        let reqsize: usize = 100_000_000;
-        let p2 = unsafe { get_devsmalloc!().realloc(p1, l1, reqsize) };
-        assert!(!p2.is_null());
-    }
-
-    fn one_alloc_slot_large() {
-        let l = Layout::from_size_align(1_000_000, 8).unwrap();
-        unsafe { get_devsmalloc!().alloc(l) };
-    }
-
     /// This reproduces a bug in `platform::plat::sys_realloc()` /
     /// `_sys_realloc_if_vm_remap_did_what_i_want()` (or possibly in MacOS's `mach_vm_remap()`) that
     /// was uncovered by tests::threads_1_large_alloc_dealloc_realloc_x()
@@ -85,8 +24,6 @@ nextest_integration_tests! {
         assert!(!p3.is_null());
     }
 
-    // xxx consider reducing the code size of these tests...
-
     fn test_alloc_1_byte_then_dealloc() {
         let layout = Layout::from_size_align(1, 1).unwrap();
         let p = unsafe { get_devsmalloc!().alloc(layout) };
@@ -94,88 +31,64 @@ nextest_integration_tests! {
         unsafe { get_devsmalloc!().dealloc(p, layout) };
     }
 
-    fn threads_1_small_alloc_x() {
+    fn threads_1_alloc_x() {
         help_test_multithreaded(1, 100, false, false, false);
     }
 
-    fn threads_1_small_alloc_dealloc_x() {
+    fn threads_1_alloc_dealloc_x() {
         help_test_multithreaded(1, 100, true, false, false);
     }
 
-    fn threads_1_small_alloc_dealloc_realloc_x() {
+    fn threads_1_alloc_dealloc_realloc_x() {
         help_test_multithreaded(1, 100, true, true, false);
     }
 
-    fn threads_1_small_alloc_dealloc_with_writes_x() {
+    fn threads_1_alloc_dealloc_with_writes_x() {
         help_test_multithreaded(1, 100, true, false, true);
     }
 
-    fn threads_1_small_alloc_dealloc_realloc_with_writes_x() {
+    fn threads_1_alloc_dealloc_realloc_with_writes_x() {
         help_test_multithreaded(1, 100, true, true, true);
     }
 
-    fn threads_2_small_alloc_x() {
+    fn threads_2_alloc_x() {
         help_test_multithreaded(2, 100, false, false, false);
     }
 
-    fn threads_2_small_alloc_dealloc_x() {
+    fn threads_2_alloc_dealloc_x() {
         help_test_multithreaded(2, 100, true, false, false);
     }
 
-    fn threads_2_small_alloc_dealloc_realloc_x() {
+    fn threads_2_alloc_dealloc_realloc_x() {
         help_test_multithreaded(2, 100, true, true, false);
     }
 
-    fn threads_2_small_alloc_dealloc_with_writes_x() {
+    fn threads_2_alloc_dealloc_with_writes_x() {
         help_test_multithreaded(2, 100, true, false, true);
     }
 
-    fn threads_2_small_alloc_dealloc_realloc_with_writes_x() {
+    fn threads_2_alloc_dealloc_realloc_with_writes_x() {
         help_test_multithreaded(2, 100, true, true, true);
     }
 
-    fn threads_32_small_alloc_x() {
-        help_test_multithreaded(32, 100, false, false, false);
+    fn threads_4096_alloc_x() {
+        help_test_multithreaded(4096, 100, false, false, false);
     }
 
-    fn threads_32_small_alloc_dealloc_x() {
-        help_test_multithreaded(32, 100, true, false, false);
+    fn threads_4096_alloc_dealloc_x() {
+        help_test_multithreaded(4096, 100, true, false, false);
     }
 
-    fn threads_32_small_alloc_dealloc_realloc_x() {
-        help_test_multithreaded(32, 100, true, true, false);
+    fn threads_4096_alloc_dealloc_realloc_x() {
+        help_test_multithreaded(4096, 100, true, true, false);
     }
 
-    fn threads_32_small_alloc_dealloc_with_writes_x() {
-        help_test_multithreaded(32, 100, true, false, true);
+    fn threads_4096_alloc_dealloc_with_writes_x() {
+        help_test_multithreaded(4096, 100, true, false, true);
     }
 
-    fn threads_32_small_alloc_dealloc_realloc_with_writes_x() {
-        help_test_multithreaded(32, 100, true, true, true);
-    }
-
-    fn threads_64_small_alloc_x() {
-        help_test_multithreaded(64, 100, false, false, false);
-    }
-
-    fn threads_64_small_alloc_dealloc_x() {
-        help_test_multithreaded(64, 100, true, false, false);
-    }
-
-    fn threads_64_small_alloc_dealloc_realloc_x() {
-        help_test_multithreaded(64, 100, true, true, false);
-    }
-
-    fn threads_64_small_alloc_dealloc_with_writes_x() {
-        help_test_multithreaded(64, 100, true, false, true);
-    }
-
-    fn threads_64_small_alloc_dealloc_realloc_with_writes_x() {
-        help_test_multithreaded(64, 100, true, true, true);
-    }
-
-    fn threads_1_medium_alloc_x() {
-        help_test_multithreaded(1, 100, false, false, false);
+    fn threads_4096_alloc_dealloc_realloc_with_writes_x() {
+        help_test_multithreaded(4096, 100, true, true, true);
     }
 }
 
