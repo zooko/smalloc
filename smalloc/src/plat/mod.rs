af1c124a4dc83fa3ac9c2bb8d2b7121cbebe09c6
Zsomething <nothanks@ontheemail.com>
2026-01-02 12:50:22 -0700

    oops forgot to git add plat

---
+++ smalloc/src/plat/mod.rs
@@ -0,0 +1,69 @@
+// Abstract over system virtual memory functions
+
+#[derive(Debug)]
+pub struct AllocFailed;
+
+impl std::error::Error for AllocFailed {}
+
+use std::fmt;
+impl fmt::Display for AllocFailed {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Alloc failed")
+    }
+}
+
+#[cfg(any(target_os = "linux", doc))]
+pub mod p {
+    use super::AllocFailed;
+    use rustix::mm::{MapFlags, ProtFlags, mmap_anonymous};
+    use std::ptr;
+
+    #[allow(unsafe_code)]
+    pub fn sys_alloc(reqsize: usize) -> Result<*mut u8, AllocFailed> {
+        match unsafe {
+            mmap_anonymous(
+                ptr::null_mut(),
+                reqsize,
+                ProtFlags::READ | ProtFlags::WRITE,
+                MapFlags::PRIVATE | MapFlags::NORESERVE,
+            )
+        } {
+            Ok(p) => Ok(p as *mut u8),
+            Err(_) => Err(AllocFailed),
+        }
+    }
+}
+
+#[cfg(any(target_vendor = "apple", doc))]
+pub mod p {
+    use super::AllocFailed;
+    use mach_sys::kern_return::KERN_SUCCESS;
+    use mach_sys::port::mach_port_t;
+    use mach_sys::traps::mach_task_self;
+    use mach_sys::vm::mach_vm_allocate;
+    use mach_sys::vm_statistics::VM_FLAGS_ANYWHERE;
+    use mach_sys::vm_types::{mach_vm_address_t, mach_vm_size_t};
+
+    #[allow(unsafe_code)]
+    pub fn sys_alloc(size: usize) -> Result<*mut u8, AllocFailed> {
+        let task: mach_port_t = unsafe { mach_task_self() };
+        let mut address: mach_vm_address_t = 0;
+        let size: mach_vm_size_t = size as mach_vm_size_t;
+
+        let retval;
+        unsafe {
+            retval = mach_vm_allocate(task, &mut address, size, VM_FLAGS_ANYWHERE);
+        }
+        if retval == KERN_SUCCESS {
+            Ok(address as *mut u8)
+        } else {
+            Err(AllocFailed)
+        }
+    }
+}
+
+// for Windows, check out VirtualAllocEx with MEM_RESERVE flag: https://learn.microsoft.com/en-us/windows/win32/memory/page-state
+// https://stackoverflow.com/questions/15261527/how-can-i-reserve-virtual-memory-in-linux?rq=1
+//xxx look into VirtualAlloc on windows and the difference between "reserve" and "commit"...
+
+// mimalloc by default madvise's transparent hugepage support *think*
