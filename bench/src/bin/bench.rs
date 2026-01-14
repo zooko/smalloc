b8e1849ebc4786d9b599337bdb3f7e1eb890db3b
Zsomething <nothanks@ontheemail.com>
2026-01-13 19:31:36 -0700

    bench: increase numbatches for more stable medians, increase num threads that can fit into slabs

--- bench/src/bin/bench.rs
+++ bench/src/bin/bench.rs
@@ -23,11 +23,11 @@ pub fn main() {
     let compare = std::env::args().any(|arg| arg == "--compare");
     let thorough = std::env::args().any(|arg| arg == "--thorough");
 
-    let num_batches = 80;
+    let num_batches = 200;
 
     println!("Using seed: {}", seed);
     
-    const THREADS_THAT_CAN_FIT_INTO_SLABS: u32 = 64;
+    const THREADS_THAT_CAN_FIT_INTO_SLABS: u32 = 32;
     const THREADS_WAY_TOO_MANY: u32 = 1024;
 
     // for benchmarks that are going to re-use space
