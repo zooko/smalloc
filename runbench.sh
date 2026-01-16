4a48d7d7d437dc7cf88b7f0f0c5c9d0921042668
Zsomething <nothanks@ontheemail.com>
2026-01-15 23:49:36 -0700

    bench: put bench results into a file inside ./tmp (where git will ignore it) until after the benchmark is complete
    This is so git won't report the working tree as dirty due to the existence of a new or changed bench results file.

--- runbench.sh
+++ runbench.sh
@@ -14,27 +14,29 @@ ARGS=$*
 
 ARGSSTR="${ARGS//[^[:alnum:]]/}"
 
-RESF=bench/results/cargo-bench.result.${CPUTYPE}.${OSTYPESTR}.${ARGSSTR}.txt
+FNAME="cargo-bench.result.${CPUTYPE}.${OSTYPESTR}.${ARGSSTR}.txt"
+TMPF="tmp/${FNAME}"
+RESF="bench/results/${FNAME}"
 
-echo "# Saving result into a file named \"${RESF}\" ..."
+echo "# Saving result into a tmp file (in ./tmp) which will be moved to \"${RESF}\" when complete..."
 
-rm -f $RESF
+rm -f $TMPF
 
-git log -1 | head -1 2>&1 | tee -a $RESF
-echo "# git log -1 | head -1" 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+git log -1 | head -1 2>&1 | tee -a $TMPF
+echo "# git log -1 | head -1" 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
 
-echo "[ -z \"\$(git status --porcelain)\" ] && echo \"Clean\" || echo \"Uncommitted changes\"" 2>&1 | tee -a $RESF
-[ -z "$(git status --porcelain)" ] && echo "Clean" || echo "Uncommitted changes" 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+echo "[ -z \"\$(git status --porcelain)\" ] && echo \"Clean\" || echo \"Uncommitted changes\"" 2>&1 | tee -a $TMPF
+[ -z "$(git status --porcelain)" ] && echo "Clean" || echo "Uncommitted changes" 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
 
-echo CPU type: 2>&1 | tee $RESF
-echo $CPUTYPE 2>&1 | tee $RESF
-echo 2>&1 | tee $RESF
+echo CPU type: 2>&1 | tee $TMPF
+echo $CPUTYPE 2>&1 | tee $TMPF
+echo 2>&1 | tee $TMPF
 
-echo OS type: 2>&1 | tee -a $RESF
-echo $OSTYPE 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+echo OS type: 2>&1 | tee -a $TMPF
+echo $OSTYPE 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
 
 if [ "x${OSTYPE}" = "xmsys" ]; then
 	# no jemalloc on windows
@@ -43,9 +45,11 @@ else
 	ALLOCATORS=mimalloc,rpmalloc,jemalloc,snmalloc
 fi
 
-cargo --locked build --release --package bench --features=$ALLOCATORS 2>&1 | tee -a $RESF
+cargo --locked build --release --package bench --features=$ALLOCATORS 2>&1 | tee -a $TMPF
 
-echo "# ./target/release/bench --compare ${ARGS}" 2>&1 | tee -a $RESF
-echo 2>&1 | tee -a $RESF
+echo "# ./target/release/bench --compare ${ARGS}" 2>&1 | tee -a $TMPF
+echo 2>&1 | tee -a $TMPF
 
-./target/release/bench --compare ${ARGS} 2>&1 | tee -a $RESF
+./target/release/bench --compare ${ARGS} 2>&1 | tee -a $TMPF
+
+mv --force "${TMPF}" "${RESF}"
