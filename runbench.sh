45727ef03b28025343a13c256638e49bbec56006
Zsomething <nothanks@ontheemail.com>
2026-01-23 17:06:48 -0700

    tidy up script and make it more similar to the comparable scripts in simd-json, rebar, and bench-allocators

--- runbench.sh
+++ runbench.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -e
+
 BNAME="cargo-bench"
 
 # Collect metadata
-GITCOMMIT="$(git log -1 | head -1 | cut -d' ' -f2)"
+GITCOMMIT=$(git rev-parse HEAD)
 GITCLEANSTATUS=$( [ -z "$( git status --porcelain )" ] && echo \"Clean\" || echo \"Uncommitted changes\" )
 TIMESTAMP=$(date -u +"%Y-%m-%d %H:%M:%S UTC")
 
 # CPU type on linuxy
-CPUTYPE=`grep "model name" /proc/cpuinfo 2>/dev/null | uniq | cut -d':' -f2-`
-if [ "x${CPUTYPE}" = "x" ] ; then
+CPUTYPE=$(grep -m1 "model name" /proc/cpuinfo 2>/dev/null | cut -d':' -f2-)
+if [ -z "${CPUTYPE}" ] ; then
     # CPU type on macos
-    CPUTYPE=`sysctl -n machdep.cpu.brand_string 2>/dev/null`
+    CPUTYPE=$(sysctl -n machdep.cpu.brand_string 2>/dev/null || echo "Unknown")
 fi
 CPUTYPESTR="${CPUTYPE//[^[:alnum:]]/}"
 OSTYPESTR="${OSTYPE//[^[:alnum:]]/}"
@@ -23,7 +25,6 @@ RESF="${OUTPUT_DIR}/${BNAME}.result.txt"
 GRAPH_BASE="${OUTPUT_DIR}/${BNAME}.graph-"
 
 mkdir -p ${OUTPUT_DIR}
-
 rm -f $RESF
 
 echo "GITCOMMIT: ${GITCOMMIT}" 2>&1 | tee -a $RESF
@@ -37,14 +38,14 @@ if [ "x${OSTYPE}" = "xmsys" ]; then
     # no jemalloc or snmalloc on windows
     ALLOCATORS=mimalloc,rpmalloc
 else
-    ALLOCATORS=mimalloc,rpmalloc,jemalloc,snmalloc
+    ALLOCATORS=jemalloc,snmalloc,mimalloc,rpmalloc
 fi
 
-cargo --locked build --release --package bench --features=$ALLOCATORS &&
+cargo --locked build --release --package bench --features=$ALLOCATORS
 
-echo "# ./target/release/bench --compare ${ARGS}" 2>&1 | tee -a $RESF &&
-echo 2>&1 | tee -a $RESF &&
-./target/release/bench --compare ${ARGS} 2>&1 | tee -a $RESF &&
+echo "# ./target/release/bench --compare ${ARGS}" 2>&1 | tee -a $RESF
+echo 2>&1 | tee -a $RESF
+./target/release/bench --compare ${ARGS} 2>&1 | tee -a $RESF
 
 # Generate graphs with sumstats.py
 ./bench/sumstats.py "$RESF" \
@@ -52,6 +53,6 @@ echo 2>&1 | tee -a $RESF &&
     --commit "$GITCOMMIT" \
     --git-status "$GITCLEANSTATUS" \
     --cpu "$CPUTYPE" \
-    --os "$OSTYPE" &&
+    --os "$OSTYPE"
 
 echo "# Data results (text) are in \"${RESF}\" ."
